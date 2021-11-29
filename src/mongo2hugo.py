ddc9bdb2270b0b5a28fffe9387754dcddff5c23e
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2021-11-29 22:38:54 +0100

    [:sparkles:] Nuevas funciones en archivo principal
    Co-authored-by: Noelia <noecrespi@users.noreply.github.com>

--- src/mongo2hugo.py
+++ src/mongo2hugo.py
@@ -1,12 +1,25 @@
+from os.path import join
+from os import getcwd
 
-def generateSite(sections: list, elements: dict) -> None:
+from src.utils.hugo.createHugoStructure import createHugoStructure
+
+from .utils.hugo.checkFolderStructure import isFolderStructure
+
+def generateSite(sections: list, elements: dict, *, outputFolder: str = join(getcwd(), "content")) -> None:
     """
-    
+    Generate site from a list of sections and
+    populate with elements inside of each section
     """
 
     # preconditions
     assert isinstance(sections, list)
     assert isinstance(elements, dict)
+    assert isinstance(outputFolder, str)
+
+    # logic
+    if not isFolderStructure(outputFolder):
+        createHugoStructure(outputFolder, sections)
+
 
 if __name__ == '__main__':
     # collect data from MongoDB
