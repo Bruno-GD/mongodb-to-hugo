946937bab00750eff09b4549c4f23382775ce4f1
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2021-11-29 20:09:01 +0100

    [:white_check_mark:] Añadido logs a pytest
    Co-authored-by: Noelia <noecrespi@users.noreply.github.com>

--- test/test_db_connection.py
+++ test/test_db_connection.py
@@ -1,8 +1,12 @@
 import pytest
 from src.utils.db.getDataFromDB import getDataFromDB
+import logging
+LOGGER = logging.getLogger(__name__)
 
 @pytest.mark.db_connection
 def test_db_connection():
     collectionNames, collections = getDataFromDB()
     assert isinstance(collectionNames, list), 'collectionNames no es una lista'
     assert isinstance(collections, dict), 'collections no es un diccionario'
+    LOGGER.info(collectionNames)
+    LOGGER.info(collections)
