92a28843cd677afec8a4f41ddfcb160fb2b731f6
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2021-11-29 22:37:56 +0100

    [:sparkles:] Creador de estructuras
    Co-authored-by: Noelia <noecrespi@users.noreply.github.com>

---
+++ src/utils/hugo/createHugoStructure.py
@@ -0,0 +1,28 @@
+from os import getcwd, mkdir
+from os.path import isdir, join
+from shutil import rmtree
+
+def createHugoStructure(hugoDir: str, sections: list, *, force: bool = False) -> None:
+    """
+    Creates a new Hugo structure at `hugoDir`
+    """
+
+    # precondition
+    assert isinstance(hugoDir, str)
+
+    # logic
+
+    if force and isdir(hugoDir):
+        rmtree(hugoDir)
+        mkdir(hugoDir)
+    elif not isdir(hugoDir):
+        mkdir(hugoDir)
+
+    for section in sections:
+        sectionDir = join(hugoDir, section)
+
+        if not isdir(sectionDir):
+            mkdir(sectionDir)
+
+        with open(join(sectionDir, 'index.md'), 'w', encoding='utf8') as file:
+            file.write('')
