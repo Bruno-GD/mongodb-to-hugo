f00c53489f2b0f0776cf930bd9bb7c714bb7a782
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2021-11-29 20:10:34 +0100

    [:recycle:] Mejora modulo obtener de BD
    Co-authored-by: Noelia <noecrespi@users.noreply.github.com>

--- src/utils/db/getDataFromDB.py
+++ src/utils/db/getDataFromDB.py
@@ -1,8 +1,14 @@
+# python native modules
 from os import getenv as getEnvironmentVariable
 from typing import Tuple, Dict
+# pymongo imports
 from pymongo import MongoClient
 from pymongo.collection import Collection
-from pymongo.errors import ConnectionFailure
+# pytest logging
+from logging import getLogger as gL
+LOGGER = gL(__name__)
+# this module imports
+from .checkMongoURI import checkMongoURI
 
 def getDataFromDB() -> Tuple[list, Dict[str, Collection]]:
     """
@@ -12,23 +18,28 @@ def getDataFromDB() -> Tuple[list, Dict[str, Collection]]:
     MONGO_URI   = getEnvironmentVariable("MONGO_URI")
     MONGO_DB    = getEnvironmentVariable("MONGO_DB")
 
+    # precondition
     assert MONGO_URI != None,   'MONGO_URI env var not set'
     assert MONGO_DB != None,    'MONGO_DB env var not set'
 
-    try:
-        CLIENT = MongoClient(MONGO_URI)         # get client instance
-        DB = CLIENT.get_database(MONGO_DB)      # get db instance
-        collections = DB.list_collection_names  # get all collections inside db
+    checkMongoURI(MONGO_URI)
 
-        collectionsData = {}
-        for collection in collections:
-            collectionsData[collection] = DB.get_collection(collection)
-    except ConnectionFailure:
-        collections = []
-        collectionsData = {}
+    # logic
+    collections = list()
+    collectionsData = dict()
+    try:
+        with MongoClient(MONGO_URI) as CLIENT:
+            DB = CLIENT.get_database(MONGO_DB)              # get db instance
+            collections = DB.list_collection_names()        # get all collections inside db
+
+            for collection in collections:
+                collectionsData[collection] = DB.get_collection(collection)
+        # should close CLIENT connection
     except Exception as e:
-        collections = []
-        collectionsData = {}
-        print('Something went wrong retreiving collections or documents:', e)
+        LOGGER.error('Something went wrong at : %s', e)
     finally:
+        # postcondition
+        assert isinstance(collections, list),       'collections no es lista'
+        assert isinstance(collectionsData, dict),   'collectionsData no es dict'
+
         return collections, collectionsData
