9dde0aaee64b97696740fb2825caab39c1031c63
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2021-11-30 20:46:42 +0100

    [:recycle:] Refactor comando hugo
    Co-authored-by: Noelia <noecrespi@users.noreply.github.com>

--- src/utils/hugo/runHugoStructure.py
+++ src/utils/hugo/runHugoStructure.py
@@ -3,7 +3,7 @@ from os import system as runCommand
 from logging import getLogger as gL
 LOGGER = gL(__name__)
 
-def runHugoStructure(hugoFolder: str):
+def runHugoStructure(hugoFolder: str) -> None:
     """
     Run Hugo command to create a new site folder
     """
@@ -13,6 +13,6 @@ def runHugoStructure(hugoFolder: str):
 
     # logic
     if isHugoAvailable():
-        runCommand("hugo new site %s" % hugoFolder)
+        LOGGER.info("Running Hugo: %s", runCommand("hugo new site %s" % hugoFolder))
     else:
         LOGGER.error('Hugo command not available')
