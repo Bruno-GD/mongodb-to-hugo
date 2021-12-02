5434a7e045b84055c3113ddbe9c68b42bb76c173
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2021-12-02 16:22:51 +0100

    [:white_check_mark:] Refactor casos test

--- test/test_generate_site.py
+++ test/test_generate_site.py
@@ -1,6 +1,15 @@
 import pytest
 from src.mongo2hugo import generateSite
 
-@pytest.mark.generate_site
+@pytest.mark.generate_site_sample
 def test_generate_site():
     generateSite(['section_one', 'section_two'], {})
+
+@pytest.mark.generate_site_db
+def test_generate_site_db():
+    # collect data from MongoDB
+    from src.utils.db.getDataFromDB import getDataFromDB
+    sections, elements = getDataFromDB()
+
+    # generate site with collected data
+    generateSite(sections, elements)
