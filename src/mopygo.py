5e1b8d871efd8554f964403f8e4ba4d09d8a04da
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2021-12-10 21:14:52 +0100

    [:recycle:] Refactor 'main'
    Co-authored-by: Noelia <noecrespi@users.noreply.github.com>

--- src/mopygo.py
+++ src/mopygo.py
@@ -1,15 +1,16 @@
 from .utils.hugo.generateSite import generateSite
 from .utils.db.getDataFromDB import getDataFromDB
 
-def main(*args):
+def main(*args, **kwargs) -> None:
     """
     MoPyGo main function.
     Retreive data from DB and generate site.
     """
 
-    sections, contentOfSections = getDataFromDB()
+    sections, contentOfSections = getDataFromDB(*args)
 
-    generateSite(sections, contentOfSections)
+    generateSite(sections, contentOfSections, **kwargs)
 
 if __name__ == '__main__':
-    main()
+    insideContentFolder = input('Nombre de carpeta de contenido? .../content/')
+    main(insideFolder=insideContentFolder)
