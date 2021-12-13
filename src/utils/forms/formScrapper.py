c8d8b1b42e54643539a1a822c0cdd43660d7c3a0
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2021-12-13 12:00:23 +0100

    [:recycle:] Refactor form scrapper
    Co-authored-by: Noelia <noecrespi@users.noreply.github.com>

--- src/utils/forms/formScrapper.py
+++ src/utils/forms/formScrapper.py
@@ -1,37 +1,25 @@
 from typing import List
+from .tokenRefresher import checkCredentials
 
 from logging import getLogger as gL
 LOGGER = gL(__name__)
 
 # google spreadsheets reqs
 from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
-from google.auth.exceptions import RefreshError
 from google.oauth2.credentials import Credentials
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 def scrapSpreadsheet(
-    SPREADSHEET_ID: str, RANGE: str = "Items!A:Z", *,
-    CREDS: dict = {},
+    SPREADSHEET_ID: str, RANGE: str, *args,
+    TOKEN_FILE: str = "token.json", **kwargs
 ) -> List:
     """
     Scrap a spreadsheet from range
     """
-    CREDENTIALS = Credentials.from_authorized_user_file('token.json', SCOPES)
+    CREDENTIALS = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
 
-    if not CREDENTIALS.valid:
-        LOGGER.warning('invalid Google credentials (invalid token or expired)')
-        if CREDENTIALS.expired and CREDENTIALS.refresh_token:
-            try:
-                CREDENTIALS.refresh(Request())
-                LOGGER.info("Refreshing token")
-            except RefreshError:
-                LOGGER.error("Couldn't refresh token")
-            else:
-                with open('token.json', 'w') as token:
-                    token.write(CREDENTIALS.to_json())
-        
+    checkCredentials(CREDENTIALS)
 
     service = build('sheets', 'v4', credentials=CREDENTIALS)
     sheet = service.spreadsheets()
